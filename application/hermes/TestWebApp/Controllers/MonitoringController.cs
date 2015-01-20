0bfad91115e1906294b1a7e1e3d6e2b0d063c41b
Jan Vesely <johny.cheerful@gmail.com>
2015-01-20 23:26:16 +0100

    Nové změny pro prezentaci

--- application/hermes/TestWebApp/Controllers/MonitoringController.cs
+++ application/hermes/TestWebApp/Controllers/MonitoringController.cs
@@ -25,33 +25,46 @@ namespace TestWebApp.Controllers
         public ActionResult Create()
         {
             HermesDBEntities hDesk = new HermesDBEntities();
-            ViewBag.Reports = hDesk.Reports;
+            ViewBag.Groups = hDesk.Groups;
             return View();
         }
         
         [HttpPost]
         [Authorize]
-        public ActionResult Create(TravelContract model, string eventName)
+        public ActionResult Create(Report model, string groupName)
         {
             HermesDBEntities hDesk = new HermesDBEntities();
             User current_user = hDesk.Users.SingleOrDefault(u => u.id == WebSecurity.CurrentUserId);
-            if (ModelState.IsValid)
+            Group group = hDesk.Groups.FirstOrDefault(g => g.name == groupName);
+
+            IEnumerable<TravelContract> contractsForPeriod = group.TravelContracts.Where(tc => tc.CreatedAt >= model.PeriodFrom && tc.CreatedAt <= model.PeriodTo);
+
+            Report report = new Report();
+            report.GeneratedAt = DateTime.Now;
+            report.Group = group;
+            report.PeriodFrom = model.PeriodFrom;
+            report.PeriodTo = model.PeriodTo;
+            report.AmountOfMoneyForPeriod = 522;
+
+            int amount = 0;
+            foreach (TravelContract travelContract in contractsForPeriod)
             {
-                TravelContract te = new TravelContract();
-                te.NameOfClient = model.NameOfClient;
-                te.Adress = model.Adress;
-                te.Quantity = model.Quantity;
-                te.TravelEvent = hDesk.TravelEvents.FirstOrDefault(td => td.Name == eventName);
-                te.Group = current_user.Groups.First();
-                hDesk.TravelContracts.Add(te);
-                hDesk.SaveChanges();
-                return RedirectToAction("Index", "TravelEvent", new { msg = "Rezervace vytvořena" }); 
+                amount += travelContract.TravelEvent.Price*travelContract.Quantity;
             }
-            else
-            {
-                ViewBag.IsNotValid = true;
-                return View(model);
-            }
+            report.AmountOfMoneyForPeriod = amount;
+            hDesk.Reports.Add(report);
+            hDesk.SaveChanges();
+            return RedirectToAction("Index", new { msg = "Report úspěšně vygenerován" });
+        }
+
+        public ActionResult Details(int id, string msg = null)
+        {
+            ViewBag.Message = msg;
+            HermesDBEntities hDesk = new HermesDBEntities();
+            Report report = hDesk.Reports.SingleOrDefault(r => r.Id == id);
+            ViewBag.ReportContractsCount =
+                report.Group.TravelContracts.Count(tc => tc.CreatedAt >= report.PeriodFrom && tc.CreatedAt <= report.PeriodTo).ToString();
+            return View(report);
         }
     }
 }
